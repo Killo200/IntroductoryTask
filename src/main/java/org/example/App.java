3e69fcccb1c5e9453c7ccdfd19f474f2813cfc94
Killo200 <gauss-nsk@yandex.ru>
2021-03-14 23:09:17 +0300

    Add JUnit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Application for unboxing string
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+    }
+
+    public static String unboxString(String str) {
+        return null;
+    }
+}
