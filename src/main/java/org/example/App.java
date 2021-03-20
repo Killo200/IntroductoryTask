606f1994b7c9f289bf84539eae86e085a8be91ed
Killo200 <gauss-nsk@yandex.ru>
2021-03-20 23:38:08 +0300

    Add solution logic and tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,97 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Application for unboxing string
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-
+public class App {
+    public static void main(String[] args) {
 
     }
 
     public static String unboxString(String str) {
-        return null;
+
+        Pattern patternValid = Pattern.compile("\\d|[a-z]|\\[|]");
+        Matcher matcherValid = patternValid.matcher(str);
+        String[] arrayOfSymbols = str.split("");
+        int leftBracket = 0;
+        int rightBracket = 0;
+        for (String s :  arrayOfSymbols) {
+
+            if (!matcherValid.find()) {
+                return "Строка не валидна";
+            }
+            if (matcherValid.group().equals("[")) {
+                leftBracket++;
+            }
+            if (matcherValid.group().equals("]")) {
+                rightBracket++;
+            }
+        }
+        if (leftBracket != rightBracket) {
+            return "Строка не валидна";
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append(str);
+
+        while (true) {
+
+            Pattern patternContainDigit = Pattern.compile("\\d+");
+            Matcher matcherContainDigit = patternContainDigit.matcher(result);
+
+            if (!matcherContainDigit.find()) {
+
+                return result.toString();
+            }
+
+            Pattern patternRightBracket = Pattern.compile("]");
+            Matcher matcherRightBracket = patternRightBracket.matcher(result);
+
+            int valueDigit = 0;
+            int indexDigit = 0;
+            int indexRightBracket = 0;
+            StringBuilder innerString = new StringBuilder();
+
+            try {
+
+                indexDigit = matcherContainDigit.start();
+                valueDigit = Integer.parseInt(matcherContainDigit.group());
+                if (matcherRightBracket.find()) {
+                    indexRightBracket = matcherRightBracket.start();
+
+                    innerString.append(result, indexDigit + matcherContainDigit.group().length() + 1, indexRightBracket);
+
+                    Matcher matcherContainInnerDigit = patternContainDigit.matcher(innerString);
+
+                    if (!matcherContainInnerDigit.find()) {
+
+                        for (int i = 1; i < valueDigit; i++) {
+                            innerString.append(result, indexDigit + matcherContainDigit.group().length() + 1, indexRightBracket);
+                        }
+
+                        result.replace(indexDigit, indexRightBracket + 1, innerString.toString());
+                    } else {
+
+                        innerString.append("]");
+                        matcherContainDigit.find();
+                        int indexInnerValue = matcherContainDigit.start(0);
+
+                        result.replace(indexInnerValue, indexRightBracket + 1, unboxString(innerString.toString())).toString();
+                    }
+                } else {
+
+                    throw new Exception();
+                }
+            } catch (Exception ex) {
+
+                return "Строка не валидна";
+            }
+        }
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,13 +1,60 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import junit.framework.TestCase;
 
-import org.junit.Test;
+public class AppTest extends TestCase {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+    public void testUnboxStringOnlyRepeatString() {
 
+        //Проверка развертывания строки состоящей только из частей кторые надо повторить
+
+        assertEquals("xyzxyzxyzfdsfdsfdsfds", App.unboxString("3[xyz]4[fds]"));
+        assertEquals("xyzxyzxyzxyzxyzxyzxyzxyzxyzxyzfdsfdsfdsfds", App.unboxString("10[xyz]4[fds]"));
+        assertEquals("xyzfdsfdsfdsfdsfdsfdsfdsfdsfdsfdsfds", App.unboxString("1[xyz]11[fds]"));
+    }
+
+    public void testUnboxStringOnlySimpleString() {
+
+        //Проверка развертывания строки не имеющей повторяющихся частей
+
+        assertEquals("abcdef", App.unboxString("abcdef"));
+
+    }
+
+    public void testUnboxStringMixString() {
+
+        //Проверка развертывания строки состоящей из частей кторые надо повторить и не повторяющиеся части
+
+        String actual = App.unboxString("3[xyz]4[fds]rt");
+        String expected = "xyzxyzxyzfdsfdsfdsfdsrt";
+        assertEquals(expected, actual);
+
+    }
+
+    public void testUnboxStringNestedString() {
+
+        //Проверка развертывания строки состоящей из частей кторые надо повторить и они имеют вложенность для повторения
+
+        String actual = App.unboxString("2[2[a]xyz]d");
+        String expected = "aaxyzaaxyzd";
+        assertEquals(expected, actual);
+    }
+
+    public void testUnboxStringMixNestedString() {
+        String actual = App.unboxString("dfa2[2[a]xyz]rt");
+        String expected = "dfaaaxyzaaxyzrt";
+        assertEquals(expected, actual);
+    }
+
+    public void testUnboxStringNotValid() {
+        assertEquals("Строка не валидна", App.unboxString(""));
+
+        assertEquals("Строка не валидна", App.unboxString("3[xyz]4]fds]"));
+        assertEquals("Строка не валидна", App.unboxString("3[]4[]fds]"));
+        assertEquals("Строка не валидна", App.unboxString("3][4][fds]"));
+        assertEquals("Строка не валидна", App.unboxString("dfa2[2[a]xyzrt"));
+        assertEquals("Строка не валидна", App.unboxString("dfa2[2]a]xyzrt"));
+        assertEquals("Строка не валидна", App.unboxString("dfa2]2[a]xyzrt"));
+        assertEquals("Строка не валидна", App.unboxString("!dfa2[2[a]xyz]rt"));
+    }
 }
